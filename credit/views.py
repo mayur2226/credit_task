a87a7fb204db340a9f1e118254f39bf9f00cbf43
mayur2226 <54096516+mayur2226@users.noreply.github.com>
2020-09-05 16:05:56 +0530

    Update views.py

--- credit/views.py
+++ credit/views.py
@@ -16,15 +16,14 @@ def trans(request):
     Frec= request.POST.get('reciever')
     Fam= request.POST.get('amount')
 
-
     FFF=list(User.objects.all().values('name'))
-    
+
     l=[]
     for ff in FFF:
         l.append(list(ff.values()))
     print(l)
     l1=reduce(lambda x,y: x+y, l)
-    
+
 
 
     up=None
@@ -33,21 +32,26 @@ def trans(request):
     trn=None
     rec=None
     sen=None
-    if (Fsend and Frec in l1):
-        if request.method == 'POST':
-            sender = request.POST.get('sender','')
-            reciever = request.POST.get('reciever','')
-            amount = request.POST.get('amount','')
-            transfer = Transfer(sender=sender,reciever=reciever,amount=amount)
-            transfer.save()
-        sen=User.objects.get(name=str(Fsend).lower()).current_credit
-        rec=User.objects.get(name=str(Frec).lower()).current_credit
-        trn=(sen-int(Fam))
-        trn2=(rec+int(Fam))
-        up=User.objects.filter(name=str(Fsend).lower()).update(current_credit=trn)
-        up2=User.objects.filter(name=str(Frec).lower()).update(current_credit=trn2)
-    elif(Fsend and Frec not in l1):
-        return HttpResponse("Please enter valid names from user list only and also check the names you entered for case sensitivity")
+    if Fam==None:
+        Fam=0
+    if (int(Fam)<0):
+        return HttpResponse("Enter valid amount")
+    else:
+        if (Fsend and Frec in l1):
+            if request.method == 'POST':
+                sender = request.POST.get('sender','')
+                reciever = request.POST.get('reciever','')
+                amount = request.POST.get('amount','')
+                transfer = Transfer(sender=sender,reciever=reciever,amount=amount)
+                transfer.save()
+            sen=User.objects.get(name=str(Fsend).lower()).current_credit
+            rec=User.objects.get(name=str(Frec).lower()).current_credit
+            trn=(sen-int(Fam))
+            trn2=(rec+int(Fam))
+            up=User.objects.filter(name=str(Fsend).lower()).update(current_credit=trn)
+            up2=User.objects.filter(name=str(Frec).lower()).update(current_credit=trn2)
+        elif(Fsend and Frec not in l1):
+            return HttpResponse("Please enter valid names from user list only and also check the names you entered for case sensitivity")
 
     return render(request, 'transaction.html', {'up':up,'up2':up,'trn2':trn2,'trn':trn,'sen':sen,'rec':rec,'l1':l1,'l':l,'FFF':FFF,'Fsend':Fsend,'Frec':Frec,'Fam':Fam})
 
